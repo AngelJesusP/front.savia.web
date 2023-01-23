80f6c9a04ddb8228123b8f68361010c9f9d16286
juan pablo <juan.morales@dation.co>
2023-01-23 08:36:56 -0500

    Subiendo cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import {
-    BrowserRouter,
-    Routes, // instead of "Switch"
-    Route,
-} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from './modules/auth/views/login'
-import { HomeMain } from './modules/home/views/home'
+import HomeMain from './modules/home/views/HomeMain'
+import { ProtectedRoute } from './utils/components/ProtectedRoute';
 
 export default function App() {
     return (
-        <div className='container-app'>
+        <div className='container-app background-login' style={{ height: '100vh' }}>
             <BrowserRouter>
                 <Routes>
                     <Route path="/savia/login/" element={<Login />} />
-                </Routes>
-                <Routes>
-                    <Route path="/savia/home/" element={<HomeMain />} />
+                    <Route path="/savia/*" element={
+                        <ProtectedRoute >
+                            <HomeMain />
+                        </ProtectedRoute>
+                    } />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ src/modules/upload/types/Consulta.type.ts
@@ -0,0 +1,22 @@
+export type MyProps = {
+    
+};
+export type MyState = {
+    jsonAlert: any,
+
+    listEnfermedades: any[],
+    listIps: any[],
+
+    columnas: any,
+
+    tabTablaConsulta: boolean,
+    tabInformacionDetalle: boolean,
+    tabLogErrores: boolean,
+
+    idEnfermedad: number,
+    idIps: number,
+    tipoDocumento: string | any,
+    numeroDocumento: string | any,
+    desde: string | any,
+    hasta: string | any
+};
